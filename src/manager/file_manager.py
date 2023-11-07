6d77e9e11bbe6b6b564db99c7744da21f42b5dba
Lukas Mandok <LukasMandok28@gmail.com>
2023-11-07 19:09:40 +0100

    improved data storage format for download_list added

--- src/manager/file_manager.py
+++ src/manager/file_manager.py
@@ -9,8 +9,8 @@ class FileManager:
         self.download_list_file = config.get_download_list_file
         self.data = None
 
-        self._load_mod_list()
-        self._load_download_list()
+        self.mod_list = self._load_mod_list()
+        self.download_mod_list, self.download_mod_dict = self._load_download_list()
         
     ### private functions
         
@@ -38,63 +38,130 @@ class FileManager:
     def _load_download_list(self):
         with open(self.download_list_file, 'r') as file:
             lines = file.readlines()
-        
+            
+        return self.parse_download_file(lines)      
         
     
-    def _save_download_list(self, data):
+    def _save_download_list(self, mods):
         pass
     
     @staticmethod
-    def parse_file(lines):
+    def parse_download_file(lines):
+        mod_dict = {}
+        mod_list = {}
+                
         current_categories = []
-        previous_indentation = 0
-        previous_line = None
-        mods = {}
+        indentation = 0
+        line = None
         intendation_steps = None
+        comment = None
         
-        def add_mod(name, categories):
-            mods[name] = categories.copy()
-        
-        for line in lines:
-            line = line.rstrip() # remove trailing whitespace
+        def add_mod(name, categories, comment = None):
+            mod_list[name] = {"categories"   : categories.copy(),
+                               "comment"      : comment}
+
+            current_dict = mod_dict
+            #categories = categories or [None]
+            for category in categories:
+                current_dict = current_dict.setdefault("category", {})
+                current_dict = current_dict.setdefault(category, {})
+            current_dict = current_dict.setdefault("mods", [])
+            current_dict.append({"name" : name, "comment" : comment})
+                    
+        for next_line in lines:
+            next_line = next_line.rstrip() # remove trailing whitespace
             
             # TODO: more preciise way to determine indentation (separate tabs from spaces)
-            indentation = len(line) - len(line.lstrip())
-            line = line.strip()
+            next_indentation = len(next_line) - len(next_line.lstrip())
+            next_line = next_line.strip()
             
             # ignore comments and emtpy lines
-            if line.startswith("#") or line is None or line == "":
+            if next_line.startswith("#") or next_line is None or next_line == "":
                 continue
             
             # ignore comment part of not-empty lines and strip again
-            line = line.split("#")[0].strip()
+            next_line, *next_comment = next_line.split("#", 1)
+            next_line = next_line.strip()
+            next_comment = next_comment[0].strip() if next_comment else None
             
             # calculate intendation difference to previous line and normalize to 1
-            diff = indentation - previous_indentation
+            diff = next_indentation - indentation
             if intendation_steps is None:
                 if diff > 0:
                     intendation_steps = diff
             else:
                 diff //= intendation_steps
                 
-            if previous_line:
+            if line:
                 if diff > 0:
                     # previous line was a category
-                    current_categories.append(previous_line)
+                    current_categories.append(line)
                     
                 elif diff < 0:
                     # prvious line was a mod
-                    add_mod(previous_line, current_categories)
+                    add_mod(line, current_categories, comment)
                     # remove |diff| number of categories
                     current_categories = current_categories[:diff]
                     
                 elif diff == 0:
                     # add mod:
-                    add_mod(previous_line, current_categories)
+                    add_mod(line, current_categories, comment)
                     
-            previous_line = line
-            previous_indentation = indentation
+            line = next_line
+            indentation = next_indentation
+            comment = next_comment
             
         # Process the last line of the file
-        if previous_line:
-            add_mod(previous_line, current_categories)
+        if line:
+            add_mod(line, current_categories, next_comment)
+            
+        return mod_dict, mod_list
+    
+    
+    # parse 
+    def generate_download_file(mods):        
+        def get_spaces(intendation):
+            return " " * intendation * 4
+    
+        # iterator over the mod_dict
+        def iterate_dict(current_dict, level = 0):
+            for key, value in current_dict.items():
+                if key == "mods":
+                    for mod in value:
+                        yield level, mod["name"], mod["comment"]
+                elif key == "category":
+                    yield from iterate_dict(value, level)
+                else:
+                    yield level, key, None
+                    yield from iterate_dict(value, level + 1)
+                    
+        
+        lines = []
+        for level, name, comment in iterate_dict(mods):
+            line = get_spaces(level) + name
+            if comment:
+                line += " # " + comment
+            lines.append(line)
+
+        
+        return lines
+        
+        # def compare_categories(categories1, categories2):
+        #     if categories1 == categories2:
+        #         return False
+        #     mask = [c1 == c2 for c1, c2 in zip(categories1, categories2)]
+        #     intendation =  mask.index(False) if False in mask else sum(mask)
+        #     return intendation
+        
+            
+        # for mod, mod_info in mod_dict.items():
+        #     categories = mod_info["categories"]
+        #     intendation = compare_categories(current_categories, categories)
+            
+        #     # Add mod with the same indentation as the last category
+        #     mod_line = ' ' * (len(mod_info["categories"]) * 4) + mod
+        #     # Add comment if it exists
+        #     if mod_info["comment"]:
+        #         mod_line += "  # " + mod_info["comment"]
+        #     lines.append(mod_line)
+        # return lines
